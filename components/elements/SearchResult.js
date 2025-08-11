4acf3138810099f8f423cef86b43afe8cf5bd9b1
hamed foroughi <foroughihamed33@gmail.com>
2025-08-11 15:07:52 +0330

    search Component completed successfully and styled

--- components/elements/SearchResult.js
+++ components/elements/SearchResult.js
@@ -1,12 +1,39 @@
-import TourCard from "./TourCard"
-import styles from "./SearchResult.module.css"
-const SearchResult = ({data:{data}}) => {
-    console.log(data)
-  return (
-    <div className={styles.container}>
-        {data.map(item => <TourCard tour={item} />)}
-    </div>
-  )
-}
+"use client";
+import { useEffect, useRef } from "react";
+import TourCard from "./TourCard";
+import styles from "./SearchResult.module.css";
 
-export default SearchResult
+const SearchResult = ({ data: { data }, setIsOpen, isOpen }) => {
+  const tourRef = useRef(null);
+
+  useEffect(() => {
+    const outSideHandler = (e) => {
+      console.log(e);
+      if (tourRef.current && !tourRef.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    };
+    if (isOpen) {
+      document.addEventListener("mousedown", outSideHandler);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", outSideHandler);
+    };
+  }, [isOpen]);
+  return (
+    <div ref={tourRef} className={styles.container}>
+      <div className={styles.closeBt}>
+        <span onClick={() => setIsOpen(false)}>X</span>
+      </div>
+      <div className={styles.cards}>
+        {!data.length && <p className={styles.fail}>محصولی یافت نشد!</p>}
+        {data.map((item) => (
+          <TourCard tour={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SearchResult;
--- components/modules/Search.js
+++ components/modules/Search.js
@@ -1,25 +1,29 @@
 "use client";
 
+import { useState } from "react";
+import toast from "react-hot-toast";
 import DatePicker from "react-multi-date-picker";
 import persian from "react-date-object/calendars/persian";
 import persian_fa from "react-date-object/locales/persian_fa";
 
-import styles from "./Search.module.css";
-import { useState } from "react";
 import { toursRoutes } from "@/utils/instants";
 import { convertCity } from "@/utils/helper";
 import { useGetSearchTours } from "@/core/services/queries";
 import SearchResult from "../elements/SearchResult";
+import styles from "./Search.module.css";
 
 const Search = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const [query, setQuery] = useState({
     originId: "",
     destinationId: "",
     startDate: "",
     endDate: "",
   });
-  const { data, isPending,isLoading , refetch } = useGetSearchTours(query);
-  console.log(data);
+  
+  const { data, isPending, isLoading, refetch } = useGetSearchTours(query);
+  
+  
   const dateHandler = (value) => {
     if (Array.isArray(value) && value.length === 2) {
       const [startDate, endDate] = value;
@@ -35,8 +39,11 @@ const Search = () => {
 
   const submitHandler = (e) => {
     e.preventDefault();
+    const { originId, destinationId, startDate } = query;
+    if (!originId.length && !destinationId.length && !startDate.length)
+      return toast.error("مقداری برای جستجو وارد کنید");
     refetch();
-    console.log(query);
+    setIsOpen(true);
   };
   const changeHandler = (e) => {
     const { name, value } = e.target;
@@ -44,44 +51,46 @@ const Search = () => {
   };
   return (
     <>
-    
-    <form className={styles.container} onSubmit={submitHandler}>
-      <div className={styles.box}>
-        <select name="originId" onChange={changeHandler}>
-          <option value="">مبدا</option>
-          {toursRoutes.map((route) => (
-            <option key={route.id} value={route.id}>
-              {convertCity(route.name)}
-            </option>
-          ))}
-        </select>
-        <select name="destinationId" onChange={changeHandler}>
-          <option value="">مقصد</option>
-          {toursRoutes.map((route) => (
-            <option key={route.id} value={route.id}>
-              {convertCity(route.name)}
-            </option>
-          ))}
-        </select>
-        {/* <input className={styles.origin} placeholder="مبدا" /> */}
-        {/* <input className={styles.destination} placeholder="مقصد" /> */}
-        <div className={styles.calendar}>
-          <DatePicker
-            range
-            rangeHover
-            onChange={dateHandler}
-            dateSeparator=" تا "
-            calendar={persian}
-            locale={persian_fa}
-            calendarPosition="bottom"
-            placeholder="تاریخ"
-          />
+      <form className={styles.container} onSubmit={submitHandler}>
+        <div className={styles.box}>
+          <select name="originId" onChange={changeHandler}>
+            <option value="">مبدا</option>
+            {toursRoutes.map((route) => (
+              <option key={route.id} value={route.id}>
+                {convertCity(route.name)}
+              </option>
+            ))}
+          </select>
+          <select name="destinationId" onChange={changeHandler}>
+            <option value="">مقصد</option>
+            {toursRoutes.map((route) => (
+              <option key={route.id} value={route.id}>
+                {convertCity(route.name)}
+              </option>
+            ))}
+          </select>
+          {/* <input className={styles.origin} placeholder="مبدا" /> */}
+          {/* <input className={styles.destination} placeholder="مقصد" /> */}
+          <div className={styles.calendar}>
+            <DatePicker
+              range
+              rangeHover
+              onChange={dateHandler}
+              dateSeparator=" تا "
+              calendar={persian}
+              locale={persian_fa}
+              calendarPosition="bottom"
+              placeholder="تاریخ"
+            />
+          </div>
         </div>
-      </div>
-      <button className={styles.serachBt}>جستوجو</button>
-    </form>
-    {isLoading && <h3>Loading...</h3>}
-    {data && !isPending && <SearchResult data={data} className={styles.result}/> }
+        <button className={styles.serachBt}>جستوجو</button>
+      </form>
+      {isLoading && <h3>Loading...</h3>}
+      
+      {data && isOpen && !isPending && (
+        <SearchResult data={data} isOpen={isOpen} setIsOpen={setIsOpen} className={styles.result} />
+      )}
     </>
   );
 };
