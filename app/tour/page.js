a30709be7727225b9d57be00e528ac28c9bd6e12
hamed foroughi <foroughihamed33@gmail.com>
2025-09-03 19:42:23 +0330

    deTailsPage styles updated and responsive

--- app/tour/page.js
+++
@@ -1,7 +0,0 @@
-
-export default async function Tour() {
-    // const { param } = await params;
-    return (
-        <div></div>
-    );
-}
--- components/templates/TourDetailsPage.js
+++ components/templates/TourDetailsPage.js
@@ -12,7 +12,7 @@ import styles from "./TourDetailsPage.module.css";
 
 export default async function TourDetailsPage({ params }) {
   const { tourId } = params;
-  
+
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}tour/${tourId}`, {
     cache: "no-cache",
   });
@@ -37,123 +37,140 @@ export default async function TourDetailsPage({ params }) {
 
   return (
     <div className={styles.container}>
-      <Image src={image} width={330} height={220} alt={title} />
-      <div className={styles.title}>
-        <h2>{title}</h2>
-        <p>{`${e2p(days)}${" "}روز و ${e2p(nights)} شب`}</p>
-      </div>
-      <div className={styles.attributes}>
-        {options.includes("راهنمای تور") && (
-          <p>
-            <Image
-              width={14}
-              height={14}
-              src="/icons/user-tick.svg"
-              alt="user"
-            />{" "}
-            تورلیدر از مبدا{" "}
-          </p>
-        )}
-        <p>
-          <Image width={14} height={14} src="/icons/map.svg" alt="user" />{" "}
-          برنامه سفر{" "}
-        </p>
-        <p>
-          <Image
-            width={14}
-            height={14}
-            src="/icons/medal-star.svg"
-            alt="user"
-          />{" "}
-          تضمین کیفیت{" "}
-        </p>
-      </div>
-      <div className={styles.tourInfo}>
-        <div className={styles.horizontalScroll}>
-          {insurance && (
-            <span className={styles.infoItems}>
-              <span>
-                بیمه{" "}
-                <Image
-                  width={16}
-                  height={19}
-                  src="/icons/security.svg"
-                  alt="insurance"
-                />
-              </span>
-              <p>بیمه {e2p(50)} هزار دیناری</p>
-            </span>
-          )}
-          <span className={styles.infoItems}>
-            <span>
-              ظرفیت{" "}
-              <Image
-                width={16}
-                height={19}
-                src="/icons/profile-2user.svg"
-                alt="capacity"
-              />{" "}
-            </span>
-            <p>حداکثر {e2p(availableSeats)} نفر</p>
-          </span>
-          <span className={styles.infoItems}>
-            <span>
-              حمل و نقل{" "}
-              <Image
-                width={16}
-                height={19}
-                src="/icons/bus.svg"
-                alt="transport"
-              />
-            </span>
-            <p>{convertVehicle(fleetVehicle)}</p>
-          </span>
-          {insurance && (
-            <span className={styles.infoItems}>
-              <span>
-                تاریخ برگشت{" "}
-                <Image
-                  width={16}
-                  height={19}
-                  src="/icons/calendar-2.svg"
-                  alt="insurance"
-                />
-              </span>
-              <span>
+      <div className={styles.webDesignContainer}>
+        <div className={styles.webTop}>
+          <div className={styles.imageCont}>
+            <Image src={image} fill alt={title} />
+          </div>
+          <div className={styles.webInfo}>
+            <div className={styles.title}>
+              <h2>{title}</h2>
+              <p>{`${e2p(days)}${" "}روز و ${e2p(nights)} شب`}</p>
+            </div>
+            <div className={styles.attributes}>
+              {options.includes("راهنمای تور") && (
+                <p>
+                  <span className={styles.iconCont}>
+                    <Image fill src="/icons/user-tick.svg" alt="user" />
+                  </span>
+                  تورلیدر از مبدا
+                </p>
+              )}
+              <p>
+                <span className={styles.iconCont}>
+                  <Image fill src="/icons/map.svg" alt="user" />
+                </span>
+                برنامه سفر
+              </p>
+              <p>
+                <span className={styles.iconCont}>
+                  <Image fill src="/icons/medal-star.svg" alt="user" />
+                </span>
+                تضمین کیفیت
+              </p>
+            </div>
+            <div className={styles.webButton}>
+              <p>
+                <span>{sp(price)}</span> تومان
+              </p>
+              <DetailBt info={data}>رزرو و خرید</DetailBt>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.tourInfo}>
+          <div className={styles.horizontalScroll}>
+            {insurance && (
+              <div className={styles.infoItems}>
+                <div>
+                  بیمه
+                  <span>
+                    <Image
+                      fill
+                      src="/icons/security.svg"
+                      alt="insurance"
+                    />
+                  </span>
+                </div>
+                <p>بیمه {e2p(50)} هزار دیناری</p>
+              </div>
+            )}
+            <div className={styles.infoItems}>
+              <div>
+                ظرفیت
+                <span>
+                  <Image
+                    fill
+                    src="/icons/profile-2user.svg"
+                    alt="capacity"
+                  />
+                </span>
+              </div>
+              <p>حداکثر {e2p(availableSeats)} نفر</p>
+            </div>
+            <div className={styles.infoItems}>
+              <div>
+                حمل و نقل
+                <span>
+                  <Image
+                    fill
+                    src="/icons/bus.svg"
+                    alt="transport"
+                  />
+                </span>
+              </div>
+              <p>{convertVehicle(fleetVehicle)}</p>
+            </div>
+
+            <div className={styles.infoItems}>
+              <div>
+                تاریخ برگشت
+                <span>
+                  <Image
+                    fill
+                    src="/icons/calendar-2.svg"
+                    alt="insurance"
+                  />
+                </span>
+              </div>
+              <span className={styles.startDate}>
                 <p>{ends.year}</p>
                 <p>{ends.month}</p>
                 <p>{ends.day}</p>
               </span>
-            </span>
-          )}
-          <span className={styles.infoItems}>
-            <span>
-              تاریخ رفت{" "}
-              <Image
-                width={16}
-                height={19}
-                src="/icons/calendar-2.svg"
-                alt="capacity"
-              />{" "}
-            </span>
-            <span className={styles.startDate}>
-              <p>{begin.year}</p>
-              <p>{begin.month}</p>
-              <p>{begin.day}</p>
-            </span>
-          </span>
-          <span className={styles.infoItems}>
-            <span>
-              مبدا{" "}
-              <Image
-                width={16}
-                height={19}
-                src="/icons/routing-2.svg"
-                alt="transport"
-              />
-            </span>
-            <p>{convertCity(origin.name)}</p>
-          </span>
+            </div>
+
+            <div className={styles.infoItems}>
+              <div>
+                تاریخ رفت
+                <span>
+                  <Image
+                    fill
+                    src="/icons/calendar-2.svg"
+                    alt="capacity"
+                  />
+                </span>
+              </div>
+              <span className={styles.startDate}>
+                <p>{begin.year}</p>
+                <p>{begin.month}</p>
+                <p>{begin.day}</p>
+              </span>
+            </div>
+            <div className={styles.infoItems}>
+              <div>
+                مبدا
+                <span>
+                  <Image
+                    fill
+                    src="/icons/routing-2.svg"
+                    alt="transport"
+                  />
+                </span>
+              </div>
+              <p>{convertCity(origin.name)}</p>
+            </div>
+          </div>
         </div>
       </div>
       <div className={styles.bottom}>
